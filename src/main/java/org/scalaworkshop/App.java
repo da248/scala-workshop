1438884bcf1b35263d34db244f0185d8cd70ea2d
Dong Won (Wynn) Ahn <da248@alumni.stanford.edu>
2015-04-28 19:38:14 -0700

    scala-project

---
+++ src/main/java/org/scalaworkshop/App.java
@@ -0,0 +1,7 @@
+package org.scalaworkshop;
+
+/**
+ * Created by ykim15 on 4/28/15.
+ */
+public class App {
+}
---
+++ src/main/java/org/scalaworkshop/examples/JavaExample.java
@@ -0,0 +1,13 @@
+package org.scalaworkshop.examples;
+
+/**
+ * Created by ykim15 on 4/28/15.
+ */
+public class JavaExample {
+    public static void main(String[] args){
+
+
+
+    }
+
+}
